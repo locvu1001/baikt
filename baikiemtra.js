074f2177db149cce4b0dfa70782925445ba36114
locvu1001 <65279899+locvu1001@users.noreply.github.com>
2022-03-19 12:54:52 +0700

    Delete baikiemtra.js

--- baikiemtra.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'):
-var app = express(():
-app.get('/contact'.funtion (req. res){
-	res.send('Hello node'):
-});
-app.listen(3000. funtion (){
-    console.log('Example app listening on port 3000!');
-});
